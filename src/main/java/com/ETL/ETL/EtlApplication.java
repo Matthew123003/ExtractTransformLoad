f28066749ff6546ce19bc1b449d33fd0c4303cca
matthew123003 <matthew123003@gmail.com>
2024-09-08 20:17:46 -0400

    Used Homebrew to install ZooKeeper and ApacheKafka, both environments run

--- src/main/java/com/ETL/ETL/EtlApplication.java
+++ src/main/java/com/ETL/ETL/EtlApplication.java
@@ -15,10 +15,12 @@ public class EtlApplication {
 	// brew install zookeeper
 	// brew services start zookeeper
 	// brew services stop zookeeper
+	// START ZOOKEEPER BEFORE STARTING KAFKA
 
 	// brew install kafka
 	// brew services start kafka
 	// brew services stop kafka
+	// STOP KAFKA BEFORE STOPPING ZOOKEEPER
 
 	// Configure Kafka for Development
 	// You may want to configure the server.properties and other Kafka settings. The configuration file can be found at:
