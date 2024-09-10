c23df0d02c43dca7ab06d644c96ffd1672723197
matthew123003 <matthew123003@gmail.com>
2024-09-10 19:43:43 -0400

    Started ZooKeeper and Kafka and confirmed thier ports

--- src/main/java/com/ETL/ETL/EtlApplication.java
+++ src/main/java/com/ETL/ETL/EtlApplication.java
@@ -16,11 +16,13 @@ public class EtlApplication {
 	// brew services start zookeeper
 	// brew services stop zookeeper
 	// START ZOOKEEPER BEFORE STARTING KAFKA
+	// ZOOKEEPER RUNS ON PORT 2181
 
 	// brew install kafka
 	// brew services start kafka
 	// brew services stop kafka
 	// STOP KAFKA BEFORE STOPPING ZOOKEEPER
+	// KAFKA RUNS ON PORT 9092
 
 	// Configure Kafka for Development
 	// You may want to configure the server.properties and other Kafka settings. The configuration file can be found at:
