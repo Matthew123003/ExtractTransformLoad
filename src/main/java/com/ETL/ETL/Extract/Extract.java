cb25e04d4e197217264deffef692c2f673085af5
matthew123003 <matthew123003@gmail.com>
2024-09-12 10:27:02 -0400

    Set up extract method

--- src/main/java/com/ETL/ETL/Extract/Extract.java
+++ src/main/java/com/ETL/ETL/Extract/Extract.java
@@ -1,5 +1,8 @@
 package com.ETL.ETL.Extract;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Properties;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
@@ -20,7 +23,15 @@ public class Extract {
 
     // Extract data from a database and send to Kafka
     public void extractDataFromDatabase() {
+        String jdbcUrl = "jdbc:mysql://localhost:3306/ff7";
+        String username = "root";
+        String password = "password";
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
         
+
     };
 
 }
