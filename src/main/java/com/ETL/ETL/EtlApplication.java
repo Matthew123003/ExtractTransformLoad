b6f8fbd51c2f232e940ce969ad358d42d2ae0af9
matthew123003 <matthew123003@gmail.com>
2024-09-15 14:07:48 -0400

    Main class and main method set up for Kafka Topic ETL

--- src/main/java/com/ETL/ETL/EtlApplication.java
+++ src/main/java/com/ETL/ETL/EtlApplication.java
@@ -2,7 +2,6 @@ package com.ETL.ETL;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import com.ETL.ETL.Extract.Extract;
 
 @SpringBootApplication
