727e2724a77203b1a0589063d954b76dadc137c3
matthew123003 <matthew123003@gmail.com>
2024-09-15 14:02:27 -0400

    Updated Transform class

--- src/main/java/com/ETL/ETL/Transform/Transform.java
+++ src/main/java/com/ETL/ETL/Transform/Transform.java
@@ -1,42 +1,20 @@
 package com.ETL.ETL.Transform;
 
-import java.util.Properties;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import com.ETL.ETL.Load.Load;
 
 public class Transform {
-    private static final String TOPIC = "transformed_ff7_topic";
-    private KafkaProducer<String, String> producer;
+    private Load load;
 
     public Transform() {
-        Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092");  // Kafka broker
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-
-        this.producer = new KafkaProducer<>(props);
+        this.load = new Load();  // Initialize the Load class
     }
 
-    // This method transforms the first and last names into full name and makes them uppercase
-    public String transformData(String firstName, String lastName) {
-        String fullName = firstName + " " + lastName;
-        return fullName.toUpperCase();  // Transformation logic: concatenating and converting to uppercase
+    // Transform the data and pass it to the Load class
+    public void transformData(int id, String firstName, String lastName) {
+        // Transformation logic: concatenate first and last names and convert to uppercase
+        String transformedData = (firstName + " " + lastName).toUpperCase();
+
+        // Send the transformed data to the Load class
+        load.loadDataToKafka(id, transformedData);
     }
-
-    // This method sends transformed data to Kafka
-    public void sendTransformedDataToKafka(int id, String transformedData) {
-        // Create the transformed record
-        String record = String.format("{\"id\": %d, \"name\": \"%s\"}", id, transformedData);
-
-        // Send the transformed data to Kafka
-        producer.send(new ProducerRecord<>(TOPIC, Integer.toString(id), record));
-
-        System.out.println("Sent transformed record to Kafka: " + record);
-    }
-
-    // Close the Kafka producer when done
-    public void close() {
-        producer.close();
-    }
-
 }
