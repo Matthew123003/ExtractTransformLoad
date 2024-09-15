1bdf6ec280ff0b2d6202c861c6ab664dfd07ffe0
matthew123003 <matthew123003@gmail.com>
2024-09-15 13:54:48 -0400

    Started Load class

--- src/main/java/com/ETL/ETL/Load/Load.java
+++ src/main/java/com/ETL/ETL/Load/Load.java
@@ -1,5 +1,24 @@
 package com.ETL.ETL.Load;
 
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+
 public class Load {
+
+    private static final String TOPIC = "ff7_topic";  // Kafka topic to load the data
+    private KafkaProducer<String, String> producer;
+
+    public Load() {
+        // Set up Kafka producer properties
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");  // Kafka broker
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        // Initialize Kafka producer
+        this.producer = new KafkaProducer<>(props);
+    }
+    
+    
 
 }
