57fef3a14de6a97503e2490379a25624b2fa4a71
matthew123003 <matthew123003@gmail.com>
2024-09-15 13:48:36 -0400

    Started Transform class

--- src/main/java/com/ETL/ETL/Transform/Transform.java
+++ src/main/java/com/ETL/ETL/Transform/Transform.java
@@ -1,5 +1,21 @@
 package com.ETL.ETL.Transform;
 
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+
 public class Transform {
+    private static final String TOPIC = "transformed_ff7_topic";
+    private KafkaProducer<String, String> producer;
+
+    public Transform() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");  // Kafka broker
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        this.producer = new KafkaProducer<>(props);
+    }
+
+    
 
 }
